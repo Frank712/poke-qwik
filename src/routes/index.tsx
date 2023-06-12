bac772e2c2e90cdc8b41474c26b46d29740ee422
Frank712 <franksuriel_7@hotmail.com>
2023-06-12 03:33:24 -0500

    global styles, main component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,47 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal(1);
+
+  const changePokemonId = $((value: number) => {
+    if (pokemonId.value + value <= 0) return;
+
+    pokemonId.value += value;
+  });
+
   return (
     <>
-      <span class="text-5xl">Hello world!</span>
+      <span class="text-2xl">Buscador</span>
+      <span class="text-9xl">{pokemonId}</span>
+
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`}
+        alt="Pokemon Sprite"
+        style={{ width: "200px", height: "200px" }}
+      />
+
+      <div class="mt-2">
+        <button
+          onClick$={() => changePokemonId(-1)}
+          class="btn btn-primary mr-2"
+        >
+          Anterior
+        </button>
+        <button onClick$={() => changePokemonId(+1)} class="btn btn-primary">
+          Siguiente
+        </button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Poke - Qwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "My first app in qwik",
     },
   ],
 };
